e0dd46cbd2a533188b2eb975a6b7640003a65595
Şeymanur Güney <seymanurgu@gmail.com>
2021-11-01 23:54:59 +0300

    18 October

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("Merhaba Kodlama.io")
+
+let konutKredileri = ["Emlak Konut Kredisi", "Kamu Konut Kredisi", "Konut Kredisi "]
+
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>"+ konutKredileri[index] +"</li>")
+}
+console.log("</ul>")
